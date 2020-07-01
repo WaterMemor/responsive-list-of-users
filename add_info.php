755b59b6667c4d9427924e49e1f83578e1231446
Nastya <60959353+WaterMemor@users.noreply.github.com>
2020-07-02 00:53:42 +0300

    Update add_info.php

--- add_info.php
+++ add_info.php
@@ -27,30 +27,63 @@ if($_POST['user']) {
   $user = $_POST['user'];
  }
 
- $result = $conn->query("SELECT id,points FROM points WHERE user = '$user'");
-if($result){
-  while($row = mysqli_fetch_assoc($result)){
-    print_r($row);
-    
-    $id = current($row);
-    echo($id);
-    $point= next($row);
-    $points = $point+$like;
-    echo($points);
-    $query =$conn->query("UPDATE points SET points='$points' WHERE id='$id'");
+
+$id = $conn->query("SELECT MAX(id) FROM points");
+while($row = mysqli_fetch_assoc($id)){
+    $MAXid = current($row);
+}
+
+$id = 1;
+if(isset($_POST['delete'])){
+while($id<=$MAXid){
+$result = $conn->query("DELETE FROM points WHERE id = '$id'");
+
+$id = $id + 1;
+}
+exit('<meta http-equiv="refresh" content="0; url=index.php" />');
+} else {
+  if(isset($_POST['clean'])){
+    $points = 0;
+    while($id<=$MAXid){
+      $result = $conn->query("UPDATE points SET points='$points' WHERE id='$id'");
+      
+      $id = $id + 1;
   }
-  if ($points == 0){
+  $points=1;
+  exit('<meta http-equiv="refresh" content="0; url=index.php" />');
+} else{
+    $result = $conn->query("SELECT id,points FROM points WHERE user = '$user'");
+    while($row = mysqli_fetch_assoc($result)){
+        print_r($row);
+        
+        $id = current($row);
+        $point= next($row);
+        if(isset($_POST['add'])){
+          $points = $point+$like;
+        }
+        if(isset($_POST['delete_point'])){
+          $points = $point-$like;
+        }
+    
+        $query =$conn->query("UPDATE points SET points='$points' WHERE id='$id'");
+        exit('<meta http-equiv="refresh" content="0; url=index.php" />');
+      }
+  }
+
+ if ($points == 0){
   $id = $conn->query("SELECT MAX(id) FROM points");
   while($row = mysqli_fetch_assoc($id)){
     $MAXid = current($row);
     $MAXid = $MAXid +1;
   }
+  $points = 1;
   $result = $conn->query("INSERT INTO points (id, user, points) VALUES ('$MAXid','$user', '$points')");
+  exit('<meta http-equiv="refresh" content="0; url=index.php" />');
  }
 
-  $id->close();
-  $conn->close();
 
+  $conn->close();
+}
 ?>
 
 
